473a1802095afdbaa206e0a9ad0e6badf7bdd6de
Ahmed-18A <ahmed.abu.21418@gmail.com>
2025-09-29 20:33:28 +0300

    second

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.list;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    ImageView imageView;
+
+    String[] imageNames = {"image1", "image2", "image3", "image4", "image5", "image6"};
+
+    int[] imageIds = {R.drawable.home, R.drawable.graduationhat, R.drawable.joystick, R.drawable.school, R.drawable.schoolbag, R.drawable.tree};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        imageView = findViewById(R.id.imageView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, imageNames);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                imageView.setImageResource(imageIds[position]);
+            }
+        });
     }
 }
